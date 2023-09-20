609dc5f51354c9a352aafba0a679f74bcfddd59e
valeri <97965785+vale-87@users.noreply.github.com>
2023-09-20 14:59:03 +0400

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+document.getElementById("demo-in").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("myget-content").classList.toggle("show");
+}
+}
+function myFunction() {
+  document.getElementById("href-id").classList.remove("close");
+}
